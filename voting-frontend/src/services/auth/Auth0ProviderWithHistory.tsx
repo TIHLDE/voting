a68fd9fc626442ea667ca665c514674bef0096de
Embret Roås <embret.roas@outlook.com>
2024-10-13 21:46:10 +0200

    register participant in callback

--- voting-frontend/src/services/auth/Auth0ProviderWithHistory.tsx
+++ voting-frontend/src/services/auth/Auth0ProviderWithHistory.tsx
@@ -1,12 +1,14 @@
 import { Auth0Provider } from '@auth0/auth0-react';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useRegisterAsParticipantMutation } from '../../__generated__/graphql-types';
 const Auth0WithHistoryProvider: React.FC = ({ children }) => {
   const navigate = useNavigate();
   const domain = process.env.REACT_APP_AUTH0_DOMAIN ?? '';
   const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID ?? '';
   const callbackUrl = process.env.REACT_APP_REDIRECT_URI;
   const audience = process.env.REACT_APP_AUTH0_AUDIENCE;
+  const [registerAsParticipant] = useRegisterAsParticipantMutation();
 
 
   
@@ -17,7 +19,14 @@ const Auth0WithHistoryProvider: React.FC = ({ children }) => {
       redirectUri={callbackUrl}
       audience={audience}
       onRedirectCallback={(appState) => {
-        navigate(appState?.returnTo || "/");
+        navigate(appState?.returnTo || "/", { replace: true });
+        if (appState?.returnTo?.startsWith("/meeting/") && appState?.returnTo?.endsWith("/register")) {
+          registerAsParticipant({
+            variables: {
+              meetingId: appState.returnTo.split("/")[1],
+            },
+          });
+        }
       }}
     >
       {children}
