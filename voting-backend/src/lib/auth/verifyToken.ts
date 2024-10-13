aeb40f497395550c3af31d1f0d9f360d167218c8
Embret Roås <embret.roas@outlook.com>
2024-10-13 18:04:33 +0200

    increased cache and cooldown times

--- voting-backend/src/lib/auth/verifyToken.ts
+++ voting-backend/src/lib/auth/verifyToken.ts
@@ -13,5 +13,8 @@ export interface DecodedToken {
 export const checkJwt = auth({
   jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`,
   audience: 'https://tihlde-voting',
+  cooldownDuration: 300_000,
+  cacheMaxAge: 300_000,
+  issuer: `https://${process.env.AUTH0_DOMAIN}/`,
   tokenSigningAlg: 'RS256'
 });
